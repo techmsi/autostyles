64d980304185f69456ddd9239f5951f72511dfca
techmsi <Sophie.Idromenos@nymag.com>
2015-06-12 12:35:22 -0400

    Test: Modified tests

--- test/test.js
+++ test/test.js
@@ -14,14 +14,14 @@ var StyleGuide = autostyles;
 
 describe('#StyleGuides', function() {
   it('create new guide object', function(done) {
-    var styles = new StyleGuide(config.files);
+    var styles = new StyleGuide(config);
     expect(styles).to.be.ok;
     done();
   });
 
 
   it('converts css file: ' + chalk.bold.underline.blue(sampleCss) + ' to object', function(done) {
-    var styles = new StyleGuide(config.files);
+    var styles = new StyleGuide(config);
     var css = styles.getParsedCss(sampleCss);
     assert.typeOf(css ,'object', 'Parsed Css is an object.');
     done();
