b1a935018de64d7220985a18aca8504219911475
techmsi <sophia.idro@gmail.com>
2020-01-26 00:16:55 -0500

    refactor: swap out prompts for lighter weight option

--- src/prompt.js
+++ src/prompt.js
@@ -1,28 +1,28 @@
-const prompts = require('prompts');
+const { prompt } = require('inquirer');
 
 const getConfigMenu = [
   {
     type: 'confirm',
     name: 'debug',
-    initial: true,
+    default: true,
     message: 'Turn debug on to get log info?'
   },
   {
     type: 'text',
     name: 'source',
-    initial: './example/my-css',
+    default: './my-css',
     message: 'Source folder'
   },
   {
     type: 'text',
     name: 'output',
-    initial: 'dist',
+    default: 'dist',
     message: 'Destination folder'
   }
 ];
 
 module.exports = async () => {
-  const configurationFields = await prompts(getConfigMenu);
+  const configurationFields = await prompt(getConfigMenu);
 
   return configurationFields;
 };
