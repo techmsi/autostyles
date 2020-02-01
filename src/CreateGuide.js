fec0263f3f4f2a5cbe1c010f9b61af0ad2fde983
techmsi <sophia.idro@gmail.com>
2020-02-01 12:32:20 -0500

    refactor: move rule mapping to CreatePage

--- src/CreateGuide.js
+++ src/CreateGuide.js
@@ -1,29 +1,11 @@
 /* Autostyles - Main Module */
 const path = require('path');
-const fs = require('fs-extra');
+const { readFileSync } = require('fs');
 const css = require('mensch');
 const walk = require('simple-walk');
 
 const CreatePage = require('./CreatePage');
 const { logConfig, logMsg, separator } = require('./helpers.js');
-const BLOCK_LEVEL_ELEMENTS = require('./blockLevelElements.js');
-
-const isBlockLevel = selector => BLOCK_LEVEL_ELEMENTS.indexOf(selector) !== -1;
-
-const mapDeclaration = ({ name, value }) => ({ property: name, value });
-
-const getSelectorAndState = ([selector = '', state = null]) => ({
-  selector,
-  state
-});
-
-const mapRules = rules =>
-  rules.map(({ selectors, declarations }, ruleIndex) => ({
-    closed: ruleIndex === 0,
-    blockLevel: isBlockLevel(selectors.toString()),
-    declarations: declarations.map(mapDeclaration),
-    ...getSelectorAndState(selectors.toString().split('.'))
-  }));
 
 class CreateGuide {
   constructor({ source, output, debug, templates = 'templates' }) {
@@ -36,7 +18,7 @@ class CreateGuide {
   getParsedCss(source) {
     separator(source);
     logMsg('\n1) Get the file from ', source);
-    const file = fs.readFileSync(source, 'utf8');
+    const file = readFileSync(source, 'utf8');
 
     logMsg('2) Parsed Css', source);
     return file ? css.parse(file) : '';
@@ -51,10 +33,9 @@ class CreateGuide {
       const title = path.basename(file);
       menu.push({ title, pageIndex });
 
-      const myCss = this.getParsedCss(file);
-      const rules = mapRules(myCss.stylesheet.rules);
-      logMsg('3) Retrieved # of Styles', rules.length);
+      const { rules } = this.getParsedCss(file).stylesheet;
 
+      logMsg('3) Retrieved # of Styles', rules.length);
       const page = new CreatePage(rules, title, pageIndex);
 
       return page;
--- src/CreatePage.js
+++ src/CreatePage.js
@@ -1,10 +1,29 @@
+const BLOCK_LEVEL_ELEMENTS = require('./blockLevelElements.js');
+
 const placeholderText = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo debitis ipsum qui nesciunt sapiente explicabo quod laboriosam sed maxime voluptas at amet ducimus eligendi, et labore dolore, nihil natus expedita.`;
+const isBlockLevel = selector => BLOCK_LEVEL_ELEMENTS.indexOf(selector) !== -1;
+
+const mapDeclaration = ({ name, value }) => ({ property: name, value });
+
+const getSelectorAndState = ([selector = '', state = null]) => ({
+  selector,
+  state
+});
 
 class CreatePage {
   constructor(rules, title, pageNumber) {
-    this.rules = rules;
+    this.rules = this.mapRules(rules);
     this.title = title;
     this.pageNumber = pageNumber;
+  }
+
+  mapRules(rules) {
+    return rules.map(({ selectors, declarations }, ruleIndex) => ({
+      closed: ruleIndex === 0,
+      blockLevel: isBlockLevel(selectors.toString()),
+      declarations: declarations.map(mapDeclaration),
+      ...getSelectorAndState(selectors.toString().split('.'))
+    }));
   }
 }
 
